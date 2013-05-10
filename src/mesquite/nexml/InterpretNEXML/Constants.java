4c37377ca6013070f9af5928900e8259edb6ddcc
daisieh_local <daisieh@gmail.com>
2013-05-10 12:32:16 -0700

    Merge branch 'TSS_handler' of https://github.com/daisieh/nexml.java into daisieh-TSS_handler

--- src/mesquite/nexml/InterpretNEXML/Constants.java
+++ src/mesquite/nexml/InterpretNEXML/Constants.java
@@ -13,13 +13,21 @@ public class Constants {
 	public static final String BasePrefix = "msq";
 	public static final String NRPrefix = "nr";
 	public static final String BeanPrefix = "bean";
+
 	public static final URI BaseURI = URI.create(BaseURIString);
 	public static final URI BeanURI = URI.create(BeanURIString);
+
 	public static final String TaxaUID  = BasePrefix + ":taxaUID";
 	public static final String TaxonUID = BasePrefix + ":taxonUID";
 
 	public static final String PREDICATES_PROPERTIES = "predicateHandlerMapping.properties";
 	public static final String NAMESPACE_PROPERTIES = "namespaceHandlerMapping.properties";
 
+	public static final String NO_RULE = "NO_RULE";
     public static final String NO_VALUE = "NO_VALUE";
+    public static final int DEFAULT_CANVAS_WIDTH = 800;
+    public static final int DEFAULT_CANVAS_HEIGHT = 800;
+    public static final int DEFAULT_FONT_SIZE = 10;
+    public static final int DEFAULT_BORDER_WIDTH = 2;
+    public static final String DEFAULT_FONT_FAMILY = "Helvetica";
 }
