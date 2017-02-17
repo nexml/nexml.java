0025900cc55c9b448724cb203818415ef6ae52bd
daisie_local <daisieh@gmail.com>
2017-02-17 11:54:46 -0800

    Build two separate maven modules, one for library and one for mesquite

---
+++ mesquite_module/src/main/java/mesquite/nexml/InterpretNEXML/AnnotationHandlers/BeanHandler.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package mesquite.nexml.InterpretNEXML.AnnotationHandlers;
+
+import java.lang.reflect.Method;
+
+import mesquite.lib.Associable;
+import mesquite.lib.Listable;
+import mesquite.nexml.InterpretNEXML.Constants;
+import mesquite.nexml.InterpretNEXML.NexmlMesquiteManager;
+
+import org.nexml.model.Annotatable;
+import org.nexml.model.Annotation;
+
+/**
+ * @author rvosa
+ *
+ */
+public class BeanHandler extends NamespaceHandler {
+	/* (non-Javadoc)
+	 * @see mesquite.nexml.InterpretNEXML.PredicateHandler#getPrefix()
+	 */
+	@Override
+	public
+	String getPrefix() {
+		return Constants.BeanPrefix;
+	}
+
+	/* (non-Javadoc)
+	 * @see mesquite.nexml.InterpretNEXML.PredicateHandler#getURIString()
+	 */
+	@Override
+	public
+	String getURIString() {
+		return Constants.BeanURIString;
+	}
+
+	@Override
+	public
+	void read(Associable associable, Listable listable, int index) {
+		String[] parts = getPredicate().split(":");
+		String setter = "set" + parts[1];
+		Object value = getValue();
+        Method method;
+        try {
+            method = associable.getClass().getMethod(setter, value.getClass());
+//                method.invoke(associable, value);
+        } catch (Exception e) {
+//                e.printStackTrace();
+        }
+	}
+
+	@Override
+	public
+	void write() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
